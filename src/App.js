74e61dcaa31ae4046a2f36e3f6fa2de9829ae4b6
Harold Beckerman <halbeckerman@yahoo.com>
2019-01-27 23:42:21 -0600

    it can do basic arithmetic functions. There are some oddball outlying cases that need to be dealt with.

--- src/App.js
+++ src/App.js
@@ -8,16 +8,21 @@ class App extends Component {
     super(props);
     this.state ={
       calcDisplay: 'THE HALCULATOR',
-      numInMem: 0,
+      numInMem: "",
       operator: "",
       numIsNeg: false,
-      justForFun: ['booger','gilbert','lamar']
+
     }
-    this.revenge = this.revenge.bind(this);
+    this.addOperator = this.addOperator.bind(this);
     this.numberClick = this.numberClick.bind(this);
+    this.allClear = this.allClear.bind(this);
   }
 
+
   numberClick(e) {
+    if(this.state.calcDisplay.indexOf("0")===1&&this.state.calcDisplay.indexOf(".")!==2){
+      this.setState({calcDisplay: this.state.calcDisplay.slice(1)})
+    }
     let input = e.target.value;
     let digit;
     switch(input) {
@@ -25,13 +30,19 @@ class App extends Component {
         // attaches or removes the minus sign and updates our numIsNeg state
         if(this.state.numIsNeg) {
           digit = this.state.calcDisplay.slice(1);
-          this.setState({numIsNeg : false});
+        return this.setState({
+          numIsNeg : false,
+          calcDisplay : digit
+        });
         } else {
           digit = `-${this.state.calcDisplay}`
-          this.setState({numIsNeg: true});
+        return this.setState({
+          numIsNeg: true,
+          calcDisplay : digit
+        });
         }
-        break;
-        //adds a period ideally once and only once
+
+        //adds a decimal point ideally once and only once
       case "." :
         this.state.calcDisplay.indexOf(".")===-1?
         digit = `${this.state.calcDisplay}${input}`:
@@ -42,42 +53,96 @@ class App extends Component {
       digit = `${this.state.calcDisplay}${input}`:
       digit = `${input}`;
     }
+    if (this.state.operator!=='') {
+      digit = `${input}`
+    }
     //prevents an unnecessary state change
     if(digit!==this.state.calcDisplay) {
       this.setState({
-        numInMem: this.state.calcDisplay,
-        calcDisplay: digit})
+        operator : "",
+        calcDisplay: digit,
+        numIsNeg : false})
       }
   }
-//can be removed note the warnings in the console when it adds items with the same key
-revenge(e) {
-  let nextStateArray = this.state.justForFun;
-  switch(e.target.value) {
-    case "booger":
-      nextStateArray.push("burp");
-      break;
-    case "lamar":
-      nextStateArray.push("Jane Fonda");
-      break
-    default:
-      nextStateArray.push(e.target.value);
+
+
+
+  addOperator(e) {
+    let input =  (e.target.value);
+    if (this.state.operator!==''||input==="=") {
+      let output = this.state.numInMem+this.state.calcDisplay;
+      // this.setState({operator : input})
+      this.evaluateString(output);
+    } else {
+      // need to write an exeption for this.state.operator==="="
+    this.setState({
+      operator : input,
+      numInMem : `${this.state.numInMem}${this.state.calcDisplay} ${input} `,
+      calcDisplay : this.state.calcDisplay
+      })
+    }
   }
-  this.setState({justForFun: nextStateArray});
-}
+
+
+  //this switch function stands to get enormous...
+  evaluateString(string) {
+    const evalArray = string.split(" ");
+    console.log(evalArray);
+    let answer = Number(evalArray[0]);
+    for(let i = 0; i<evalArray.length; i++){
+      if(isNaN(Number(evalArray[i]))) {
+        switch(evalArray[i]) {
+          case "+":
+            answer+=Number(evalArray[i+1]);
+            break;
+          case "-":
+            answer-=Number(evalArray[i+1]);
+            break;
+          case "*":
+            answer = answer*Number(evalArray[i+1]);
+            break;
+          case "/":
+            answer = answer/Number(evalArray[i+1]);
+            break;
+          default:
+            console.log("What did you do?!?!?!");
+        }
+      }
+    }
+    answer = answer.toString();
+    this.setState({
+      calcDisplay : answer,
+      numInMem : ""
+    });
+ }
+
+ allClear () {
+   this.setState({
+     calcDisplay: "0",
+     numInMem: "",
+     operator: "",
+     numIsNeg: false,
+   })
+ }
 
   render() {
+    const operatorArray = ["+","-","*","/","="]
     const numbersArray = [1,2,3,4,5,6,7,8,9,".",0,"+/-"]
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="display-box" id="title">{this.state.calcDisplay}</h1>
           <ButtonPad
+          buttonFunction={this.allClear}
+          typeArray={["AC"]}
+          />
+          <ButtonPad
           buttonFunction={this.numberClick}
           typeArray={numbersArray}
           />
           <ButtonPad
-          buttonFunction={this.revenge}
-          typeArray={this.state.justForFun}
+          buttonFunction={this.addOperator}
+          typeArray={operatorArray}
           />
 
         </header>
